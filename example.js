d4e6303d24d921c718f999052a50f3b5ca73bbc9
dailcoyote <dailcoyote@gmail.com>
2019-07-23 15:59:42 +0600

    fixes & updates

--- example.js
+++ example.js
@@ -2,6 +2,11 @@ const nbRSSParser = require('./index');
 
 // Usage
 const kztValue = 45000;
-nbRSSParser(kztValue)
+nbRSSParser.calculateExchRates(kztValue)
+    .then((data) => console.log(data))
+    .catch((e) => console.log(e))
+
+// Usage
+nbRSSParser.parseExchRates()
     .then((data) => console.log(data))
     .catch((e) => console.log(e))
--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const parser = require('rss-parser-browser');
 
-function rssParseAndConvert(kztValue) {
-    let rates = {};
-    return new Promise(function(resolve, reject) {
-        parser.parseURL('http://www.nationalbank.kz/rss/rates_all.xml', function(err, parsed) {
-          if(err){
-            //console.log(err);   // or Alert
-            return reject(err);
-          }
-          //console.log(parsed.feed);
-          parsed.feed.entries.forEach(function(entry) {
-                if(entry.title === "RUB" || entry.title === "USD") {
-                    rates[entry.title] = kztValue ? (kztValue / parseInt(entry.content)).toFixed(2) : parseInt(entry.content);
-                }
-          })
-          return resolve(rates);  
+const nbRSSParser = {
+    loadFeed() {
+        return new Promise(function (resolve, reject) {
+            parser.parseURL('http://www.nationalbank.kz/rss/rates_all.xml', function (err, parsed) {
+                if (err) return reject(err);
+                else return resolve(parsed);
+            })
+        });
+    },
+    parseExchRates() {
+        let rates = {};
+        return new Promise((resolve, reject) => {
+            this.loadFeed()
+                .then((parsed) => {
+                    parsed.feed.entries.forEach(function (entry) {
+                        if (entry.title === "RUB" || entry.title === "USD") {
+                            rates[entry.title] = parseFloat(entry.content);
+                        }
+                    })
+                    return resolve(rates);
+                })
+                .catch(reject.bind(reject))
         })
-    })
+    },
+    calculateExchRates(kztValue = 0) {
+        return new Promise((resolve, reject) => {
+            this.parseExchRates()
+                .then((rates) => {
+                    for (const key in rates) {
+                        rates[key] = parseFloat((kztValue / parseFloat(rates[key])).toFixed(2));
+                    }
+                    return resolve(rates);
+                })
+                .catch(reject.bind(reject))
+        })
+    }
 }
 
-module.exports = rssParseAndConvert;
+module.exports = nbRSSParser;
